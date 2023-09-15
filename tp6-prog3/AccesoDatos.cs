0a153500e73dd9104eea48419d595735cc5f0f94
Juan <juanivelesquen@gmail.com>
2023-09-15 13:15:26 -0300

    Agrego la clase Conexion que es la misma del TP anterior solo que le cambio el nombre a AccesoDatos para que se entienda mejor la funcion principal de la clase

---
+++ tp6-prog3/AccesoDatos.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace tp5_prog3
+{
+    public class AccesoDatos
+    {
+        private string ruta;
+        private SqlConnection conexion;
+
+        public AccesoDatos(string baseDeDatos)
+        {  ///RUTA DAVID
+           ///ruta = "Data Source = DESKTOP - CIET1TI\\SQLEXPRESS01; Initial Catalog ="+ baseDeDatos +"; Integrated Security = True";
+            ruta = "Data Source = localhost\\sqlexpress; Initial Catalog = " + baseDeDatos + "; Integrated Security = True";
+            conexion = new SqlConnection(ruta);
+        }
+
+        public DataTable ObtenerTablas(string consultaSQL, string nombreTabla)
+        {
+            AbrirConexion();
+
+            SqlDataAdapter adapter = new SqlDataAdapter(consultaSQL, conexion);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, nombreTabla);
+
+            CerrarConexion();
+            return ds.Tables[nombreTabla];
+        }
+
+        public int EjecutarConsulta(string consulta)
+        {
+            AbrirConexion();
+
+            SqlCommand comando = new SqlCommand(consulta, conexion);
+            int filasAfectadas = comando.ExecuteNonQuery();
+
+            CerrarConexion();
+            return filasAfectadas;
+        }
+
+        private void AbrirConexion()
+        {
+            conexion.Open();
+        }
+
+        private void CerrarConexion()
+        {
+            conexion.Close();
+        }
+
+    }
+}
